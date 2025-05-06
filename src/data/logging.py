fda566fd8a6072739b726d8d0a0ef3e07156af5d
Liam Odero <liam.odero1@gmail.com>
2025-05-06 17:25:57 -0400

    logging.py flake8 style fixes

--- src/data/logging.py
+++ src/data/logging.py
@@ -1,40 +1,58 @@
+import os
+from numpy import ndarray, dtype, float64
 from pydub import AudioSegment
 from pydub.utils import make_chunks
 import numpy as np
-from rain_mixing.src.utils.utils import *
+from rain_mixing.src.utils.utils import wrapped_is_file
+from rain_mixing.src.utils.utils import (INPUT_DATA_DIR, OUTPUT_DATA_DIR,
+                                         S_TO_MS)
 
 CHUNKS = 100
 
 """
-Takes in an unmodified track and it's edit and saves it to the corresponding logging folder
+Takes in an unmodified track and it's edit and saves it to the corresponding
+logging folder
 
 :param
     -   input_track: An unmodified track that the user has edited
     -   output_track: The edited version of <input_track>
 """
-def log_edit(input_track: AudioSegment, output_track: AudioSegment):
-    curr_length = len([name for name in os.listdir(INPUT_DATA_DIR) if wrapped_is_file(INPUT_DATA_DIR, name)])
 
-    input_filename = os.path.join(INPUT_DATA_DIR, f"input_{curr_length}.mp3")
+
+def log_edit(input_track: AudioSegment, output_track: AudioSegment):
+    curr_length = len([name for name in os.listdir(INPUT_DATA_DIR) if
+                       wrapped_is_file(INPUT_DATA_DIR, name)])
+
+    input_filename = os.path.join(INPUT_DATA_DIR,
+                                  f"input_{curr_length}.mp3")
     input_track.export(input_filename)
 
-    output_filename = os.path.join(OUTPUT_DATA_DIR, f"output_{curr_length}.mp3")
+    output_filename = os.path.join(OUTPUT_DATA_DIR,
+                                   f"output_{curr_length}.mp3")
     output_track.export(output_filename)
 
 
 """
-Takes in the completed edits and saves <CHUNKS> dBFS samples from each corresponding input and output track
-This is mainly intended to be used as input for non-transformers models
+Takes in the completed edits and saves <CHUNKS> dBFS samples from each
+corresponding input and output track This is mainly intended to be used as
+input for non-transformers models
 
 :param
-    -   input_tracks: An ordered list of all original tracks that were edited 
-    -   output_tracks: An ordered list of all edited tracks. Each index corresponds to the same track in <input_tracks>
-    
+    -   input_tracks: An ordered list of all original tracks that were edited
+    -   output_tracks: An ordered list of all edited tracks. Each index
+    corresponds to the same track in <input_tracks>
+
 :return
-    -   input_dbfs: A 2D npy list of dBFS data that is dimension len(input_tracks) x <CHUNKS>
-    -   output_dbfs: A 2D npy list of dBFS data that is dimension len(input_tracks) x <CHUNKS>
+    -   input_dbfs: A 2D npy list of dBFS data that is dimension
+    len(input_tracks) x <CHUNKS>
+    -   output_dbfs: A 2D npy list of dBFS data that is dimension
+    len(input_tracks) x <CHUNKS>
 """
-def chunk_dbfs(input_tracks: list[AudioSegment], output_tracks: list[AudioSegment]):
+
+
+def chunk_dbfs(input_tracks: list[AudioSegment],
+               output_tracks: list[AudioSegment]) -> (
+        tuple)[ndarray[dtype[float64]], ndarray[dtype[float64]]]:
     input_dbfs = np.empty((len(input_tracks), CHUNKS))
     output_dbfs = np.empty((len(output_tracks), CHUNKS))
     # Getting chunked dBFS per each track
@@ -42,7 +60,7 @@ def chunk_dbfs(input_tracks: list[AudioSegment], output_tracks: list[AudioSegmen
         input_track = input_tracks[i]
         output_track = output_tracks[i]
 
-        # Computing how long to make each chunk to ensure that there will be <CHUNKS> many
+        # Ensure that there will be <CHUNKS> many audio chunks
         length = input_track.duration_seconds * S_TO_MS
         chunk_length = np.ceil(length / CHUNKS)
 
@@ -56,13 +74,17 @@ def chunk_dbfs(input_tracks: list[AudioSegment], output_tracks: list[AudioSegmen
 
     return input_dbfs, output_dbfs
 
+
 """
-Reads data from the logging directory and creates an array of input and output tracks
+Reads data from the logging directory and creates an array of input and
+output tracks
 
 :return
-    -   input_data: Unmodified tracks that have been selected by users 
+    -   input_data: Unmodified tracks that have been selected by users
     -   output_data: Tracks that have had they volumes modified by users
 """
+
+
 def read_logging_data():
     input_data = []
     for name in os.listdir(INPUT_DATA_DIR):
