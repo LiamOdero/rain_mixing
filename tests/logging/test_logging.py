5a983c1d8828cfc9bbbe7b56eb659706cfedfba5
Liam Odero <liam.odero1@gmail.com>
2025-05-06 19:30:52 -0400

    Testcase import fixes

--- tests/logging/test_logging.py
+++ tests/logging/test_logging.py
@@ -4,9 +4,9 @@ from pydub import AudioSegment
 
 
 def test_log_edit() -> None:
-    print(os.path.dirname(__file__))
+    test_file_path = os.path.join(os.path.dirname(__file__), "rain_sfx.mp3")
     test_input = AudioSegment.from_file(
-        file="./tests/logging/rain_sfx.mp3", format="mp3")
+        file=test_file_path, format="mp3")
     test_output = test_input + 5
     log_edit(test_input, test_output)
     assert 1 == 1
