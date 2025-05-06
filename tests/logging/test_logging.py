41cb976d684e65634351b15c75173442c0875fda
Liam Odero <liam.odero1@gmail.com>
2025-05-06 19:41:03 -0400

    ffmpeg workflow fixes?

--- tests/logging/test_logging.py
+++ tests/logging/test_logging.py
@@ -1,10 +1,13 @@
 import os
 from rain_mixing.data.logging import log_edit
+from rain_mixing.utils.utils import verify_logging_setup
 from pydub import AudioSegment
 
 
 def test_log_edit() -> None:
+    verify_logging_setup()
     test_file_path = os.path.join(os.path.dirname(__file__), "rain_sfx.mp3")
+    print(os.listdir())
     test_input = AudioSegment.from_file(
         file=test_file_path, format="mp3")
     test_output = test_input + 5
