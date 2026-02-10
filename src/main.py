66be199832379da058054cdc48a8683630074d21
Liam Odero <liam.odero1@gmail.com>
2026-02-09 20:59:14 -0500

    simplified requirements and misc. release changes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+import multiprocessing
 from rain_mixing.frontend.Window import Window
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
     print("Loading files...")
     Window().mainloop()
